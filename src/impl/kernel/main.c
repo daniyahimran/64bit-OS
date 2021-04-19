a2cf3a5f1235c8c7b713c42bdc7aa7861e325caa
Daniyah Imran <62251321+daniyahimran@users.noreply.github.com>
2021-04-19 16:17:58 +0500

    Create main.c

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,22 @@
+#include "print.h"
+
+void kernel_main() {
+    print_clear();
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("Daniyah Imran - BS CS 9B - 287498");
+    print_str("\n\n\n\n\n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("*     *    *       *    *******   *********\n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("**    *    *       *    *             *\n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("* *   *    *       *    *             *\n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("*  *  *    *       *    *******       *\n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("*   * *    *       *          *       *\n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("*     *    *       *          *       *\n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("*     *    * * * * *    *******       *\n");
+}
