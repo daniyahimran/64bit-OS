d89dd7bdf589d6f71894449a7142b5c5e94e094b
Daniyah Imran <62251321+daniyahimran@users.noreply.github.com>
2021-04-20 22:03:08 +0500

    Update main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -5,18 +5,12 @@ void kernel_main() {
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("Daniyah Imran - BS CS 9B - 287498");
     print_str("\n\n\n\n\n");
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    
     print_str("*     *    *       *    *******   *********\n");
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("**    *    *       *    *             *\n");
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("* *   *    *       *    *             *\n");
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("*  *  *    *       *    *******       *\n");
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("*   * *    *       *          *       *\n");
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("*     *    *       *          *       *\n");
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("*     *    * * * * *    *******       *\n");
 }
