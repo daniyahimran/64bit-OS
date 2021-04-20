bb744f387156dbae8ac26a3d372a7ed71ca704c8
Daniyah Imran <62251321+daniyahimran@users.noreply.github.com>
2021-04-20 23:07:57 +0500

    Update header.asm

--- src/impl/x86_64/boot/header.asm
+++ src/impl/x86_64/boot/header.asm
@@ -1,15 +1,15 @@
 section .multiboot_header
 header_start:
-	; magic number
+	; magic number, to set boot type
 	dd 0xe85250d6 ; multiboot2
 	; architecture
 	dd 0 ; protected mode i386
 	; header length
 	dd header_end - header_start
-	; checksum
+	; sum of all data variables defined above 
 	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
-	; end tag
+	; end tag - to signify that no code is coming after this
 	dw 0
 	dw 0
 	dd 8
