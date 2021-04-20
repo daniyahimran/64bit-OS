00198800f68873811f7b9465b0bc2e0c3d1c2ec4
Daniyah Imran <62251321+daniyahimran@users.noreply.github.com>
2021-04-20 23:11:53 +0500

    Update main64.asm

--- src/impl/x86_64/boot/main64.asm
+++ src/impl/x86_64/boot/main64.asm
@@ -2,9 +2,10 @@ global long_mode_start
 extern kernel_main
 
 section .text
+; setting OS operating bits as 64 for 64-bit system
 bits 64
 long_mode_start:
-    ; load null into all data segment registers
+    ; load null into all data segment registers, so all instructions run correctly
     mov ax, 0
     mov ss, ax
     mov ds, ax
@@ -12,5 +13,6 @@ long_mode_start:
     mov fs, ax
     mov gs, ax
 
+; calling function from main.c to print to console
 	call kernel_main
     hlt
